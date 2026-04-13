d0dd9c40a9b8e7afa8f482f16381d5ebee29643f
Jazz Singh <1337jazz:gmail.com>
2026-04-13 12:27:58 +0200

    fix: update selector for new user page update

--- cmd/ratiogoblin/main.go
+++ cmd/ratiogoblin/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/1337jazz/ratio-goblin/internal/config"
 	"github.com/1337jazz/ratio-goblin/internal/scraper"
+	"github.com/1337jazz/ratio-goblin/internal/updater"
 	"github.com/1337jazz/ratio-goblin/internal/version"
 )
 
@@ -31,9 +32,24 @@ func main() {
 			fmt.Println("Failed to load config - did you run `ratiogoblin init` first?\n\nERROR:", err)
 			os.Exit(1)
 		}
+		updateCh := make(chan bool, 1)
+		go func() { updateCh <- updater.HasUpdate() }()
+
 		s := scraper.NewScraper(cfg)
 		ratio := s.ScrapeRatio()
-		fmt.Println(ratio)
+
+		// Block and wait for the update check to finish before printing the ratio
+		if <-updateCh {
+			fmt.Println(ratio + " *")
+		} else {
+			fmt.Println(ratio)
+		}
+	case "update":
+		if err := updater.Update(); err != nil {
+			fmt.Println("Update:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Updated successfully.")
 	case "version":
 		printVersion()
 	default:
@@ -48,6 +64,7 @@ Commands:
   help            Show this help message
   init            Initialize configuration file with default settings
   run             Execute the ratio scraper and display the output
+  update          Update to the latest release
   version         Display version information`)
 }
 
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.25.6
 require (
 	github.com/PuerkitoBio/goquery v1.11.0 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf // indirect
 	golang.org/x/net v0.47.0 // indirect
 )
